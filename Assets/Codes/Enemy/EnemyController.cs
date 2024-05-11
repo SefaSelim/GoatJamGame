ccc47d18e60164d0e39b2809f39628b116b3b210
turanak111 <turanak111@gmail.com>
2024-05-12 01:42:47 +0300

    kakakwrqkwrwr

--- Assets/Codes/Enemy/EnemyController.cs
+++ Assets/Codes/Enemy/EnemyController.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public bool attacking = false;
+    public GameObject attackarea;
+    private Collider2D attackAreacollider;
+    private Transform areaTransform;
     public Animator enemyAnimator;
     private float Timer;
     private Rigidbody2D rb;
@@ -20,10 +24,16 @@ public class EnemyController : MonoBehaviour
     private float minrange;
     [SerializeField]
     private float attackRange;
+
+    private float timetoAttack = 0.25f;
+    private float timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        attackAreacollider = transform.GetChild(0).GetComponent<Collider2D>();
+        areaTransform = transform.GetChild(0);
+        rb = GetComponent<Rigidbody2D>();
         target = FindObjectOfType<CharacterMovement>().transform;
         Timer = 0;
 
@@ -32,68 +42,90 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (attacking)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timetoAttack)
+            {
+                timer = 0;
+                attacking = false;
+                 attackAreacollider.enabled = false;
+                
+            }
+
+        }
         Timer += Time.deltaTime;
         if (Vector3.Distance(target.position, transform.position) <= maxrange && Vector3.Distance(target.position, transform.position) >= minrange)
         { followPlayer(); }
         else if (Vector3.Distance(target.position, transform.position) >= maxrange)
         {
-             rb.velocity = Vector2.zero;
-             GoHome();
+            rb.velocity = Vector2.zero;
+            GoHome();
 
-             }
-             if (Vector3.Distance(target.position, transform.position)<attackRange&&Timer>1)
-             {
-                    enemyAnimator.SetBool("Attacking",true);
-                    //hittin here
-                    Timer = 0;
+        }
+        if (Vector3.Distance(target.position, transform.position) < attackRange && Timer > 1)
+        {
+            enemyAnimator.SetBool("Attacking", true);
+            Attack();
+            Timer = 0;
 
-             }
-             else
-             {
-                enemyAnimator.SetBool("Attacking",false);
-             }
+        }
+        else
+        {
+         
+            enemyAnimator.SetBool("Attacking", false);
+        }
 
-//asagi yön
-if(target.position.y-transform.position.y<0&&Mathf.Abs(target.position.y-transform.position.y)>Mathf.Abs(target.position.x-transform.position.x))
-{
-    enemyAnimator.SetBool("WalkingDown",true);
-}
-else
-{
-    enemyAnimator.SetBool("WalkingDown",false);
-}
+        //asagi yön
+        if (target.position.y - transform.position.y < 0 && Mathf.Abs(target.position.y - transform.position.y) > Mathf.Abs(target.position.x - transform.position.x))
+        {
+            areaTransform.rotation = Quaternion.Euler(0f, 0f, 270f);
+            areaTransform.position = new Vector3(transform.position.x - 0.3f, transform.position.y - 2.2f, areaTransform.position.z);
+            enemyAnimator.SetBool("WalkingDown", true);
 
-//yukarı yön
-if(target.position.y-transform.position.y>0&&Mathf.Abs(target.position.y-transform.position.y)>Mathf.Abs(target.position.x-transform.position.x))
-{
-    enemyAnimator.SetBool("WalkingUp",true);
-}
-else
-{
-    enemyAnimator.SetBool("WalkingUp",false);
-}
+        }
+        else
+        {
+            enemyAnimator.SetBool("WalkingDown", false);
+        }
 
-
-//sag yon
-if(target.position.x-transform.position.x>0&&Mathf.Abs(target.position.x-transform.position.x)>Mathf.Abs(target.position.y-transform.position.y))
-{
-    enemyAnimator.SetBool("WalkingRight",true);
-}
-else
-{
-    enemyAnimator.SetBool("WalkingRight",false);
-}
+        //yukarı yön
+        if (target.position.y - transform.position.y > 0 && Mathf.Abs(target.position.y - transform.position.y) > Mathf.Abs(target.position.x - transform.position.x))
+        {
+            areaTransform.rotation = Quaternion.Euler(0f, 0f, 90f);
+            areaTransform.position = new Vector3(transform.position.x + 0.4f, transform.position.y + 2.2f, areaTransform.position.z);
+            enemyAnimator.SetBool("WalkingUp", true);
+        }
+        else
+        {
+            enemyAnimator.SetBool("WalkingUp", false);
+        }
 
 
-//sol yon
-if(target.position.x-transform.position.x<0&&Mathf.Abs(target.position.x-transform.position.x)>Mathf.Abs(target.position.y-transform.position.y))
-{
-    enemyAnimator.SetBool("WalkingLeft",true);
-}
-else
-{
-    enemyAnimator.SetBool("WalkingLeft",false);
-}
+        //sag yon
+        if (target.position.x - transform.position.x > 0 && Mathf.Abs(target.position.x - transform.position.x) > Mathf.Abs(target.position.y - transform.position.y))
+        {
+            areaTransform.rotation = Quaternion.Euler(0f, 0f, 360f);
+            areaTransform.position = new Vector3(transform.position.x + 1.4f, transform.position.y - 0.2f, areaTransform.position.z);
+            enemyAnimator.SetBool("WalkingRight", true);
+        }
+        else
+        {
+            enemyAnimator.SetBool("WalkingRight", false);
+        }
+
+
+        //sol yon
+        if (target.position.x - transform.position.x < 0 && Mathf.Abs(target.position.x - transform.position.x) > Mathf.Abs(target.position.y - transform.position.y))
+        {
+            areaTransform.rotation = Quaternion.Euler(0f, 0f, 180f);
+            areaTransform.position = new Vector3(transform.position.x - 1.4f, transform.position.y + 0.3f, areaTransform.position.z);
+            enemyAnimator.SetBool("WalkingLeft", true);
+        }
+        else
+        {
+            enemyAnimator.SetBool("WalkingLeft", false);
+        }
 
 
 
@@ -107,6 +139,11 @@ else
     public void GoHome()
     {
         transform.position = Vector3.MoveTowards(transform.position, homepos.position, speed * Time.deltaTime);
+    }
+    private void Attack()
+    {
+        attackAreacollider.enabled = true;
+        attacking = true;
 
     }
 }
