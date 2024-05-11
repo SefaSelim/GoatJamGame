c51d9b8ceda1c858dad71a34d89b7191df3c96ea
SefaSelim <sefaselimtura@gmail.com>
2024-05-11 17:43:21 +0300

    Selam

--- Assets/Codes/InteractableObject.cs
+++ Assets/Codes/InteractableObject.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class InteractableObject : CollidableObject
 {   
+    bool isInside = false;
     public Animator animator;
-    private bool z_Interacted = false;
+    private bool z_Interacted = true;
 
     float timer=0;
 
@@ -15,22 +16,34 @@ public class InteractableObject : CollidableObject
        if(Input.GetKey(KeyCode.E))
        {
         OnInteract();
+        isInside = true;
 
-       }       
-        if(timer > 1)
+       } 
+        if(isInside&&(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.S)))
+        {
+            timer = 0;
+            animator.SetBool("isCollecting",false);
+            isInside = false;
+            z_Interacted = true;
+        }
+
+
+
+        if(timer > 1&&isInside)
             {
                 timer = 0;
                 animator.SetBool("isCollecting",false);
                 Destroy(gameObject);
+                isInside = false;
             
             }
 
     }
     protected virtual void OnInteract()
     {
-        if(z_Interacted == false)
+        if(z_Interacted)
         {
-            z_Interacted = true;
+            z_Interacted = false;
             Debug.Log("Interact with" + name);
             animator.SetBool("isCollecting",true);
             timer = 0;
